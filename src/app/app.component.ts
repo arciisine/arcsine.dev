0c00ea5155ebdceec4380b92bb34bbd7e71637ad
Timothy Soehnlin <tim@eaiti.com>
2019-02-04 21:59:55 -0500

    Styling for layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, HostBinding, Input, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'arcsine-pro';
+
+  constructor(private target: ElementRef) {
+    this.onScroll();
+  }
+
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+  onScroll() {
+    const mx = (window.innerHeight / 2);
+    const pct = Math.trunc(Math.min(window.scrollY / mx, 1) * 10) * 10;
+    (this.target.nativeElement as HTMLElement).setAttribute('delta', `${pct}`);
+  }
 }
