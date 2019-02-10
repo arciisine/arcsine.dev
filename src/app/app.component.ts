6bb228f991235e3d8de96a0fa5728b0461c9052a
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2019-02-10 15:07:15 -0500

    Working on landing content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'arcsine-pro';
+
+  constructor() {
+    setTimeout(() => {
+      if (window.location.hash) {
+        const anchor = window.location.hash.replace(/^#?/, '');
+        const target = (document.getElementById(anchor) || document.getElementsByName(anchor)[0]) as HTMLElement;
+        if (target) {
+          target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+      }
+    }, 250);
+  }
 }
